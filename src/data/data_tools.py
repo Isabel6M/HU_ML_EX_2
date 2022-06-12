6ff19263cf704d886bfe8278d0c4f13c092922a8
Isabel <isabel_meijer@hotmail.com>
2022-06-12 10:40:17 +0000

    excercise dataloader

--- src/data/data_tools.py
+++ src/data/data_tools.py
@@ -1,226 +1,194 @@
-# from __future__ import annotations
+from __future__ import annotations
 
-# import random
-# import shutil
-# from datetime import datetime
-# from pathlib import Path
-# from typing import Callable, Dict, Iterator, List, Optional, Sequence, Tuple, Union
+import random
+import shutil
+from datetime import datetime
+from pathlib import Path
+from typing import Callable, Dict, Iterator, List, Optional, Sequence, Tuple, Union
 
-# import numpy as np
-# import tensorflow as tf
-# import torch
-# from loguru import logger
-# from torch.nn.utils.rnn import pad_sequence
-# from tqdm import tqdm
+# import arff file
+from scipy.io import arff
 
-# Tensor = torch.Tensor
+# padding
+import torch.nn.functional as F
 
-# class BaseDataset:
-#     """The main responsibility of the Dataset class is to load the data from disk
-#     and to offer a __len__ method and a __getitem__ method
-#     """
-
-#   def __init__(self, paths: List[Path])-> None:
-#     self.paths = paths
-#     #random.shuffle(self.paths)
-#     self.dataset = []
-#     self.process_data()
-
-# def process_data(self) -> None:
-#         for file in tqdm(self.paths):
-#             x = torch.tensor(file).type(torch.numeric)
-#             y = torch.tensor(file).type(torch.nominal)
-#             self.dataset.append((x, y))
- 
-
-#   def __iter__(self):
-#     # startindex; this makes the first index 0
-#     self.idx = -1
-#     # we return the full object when iter() is called
-#     return self
-
-#   def __next__(self):
-#     # for every iteration, __next__ is called
-#     # as long as the idx is not bigger than the data
-#     # we need to do -1, because we will increase idx directly after this
-#     if self.idx < len(self.data) - 1:
-#       self.idx += 1
-#       return self.data[self.idx]
-#     else:
-#       raise StopIteration
+import numpy as np
+import tensorflow as tf
+import torch
 
 
-# myclass = BaseIterator(n=5)
-# myiter = iter(myclass) # this calles the __iter__ method and sets idx to -1
-
-# for x in myiter: # this calls the __next__ method
-#   print(x)
+Tensor = torch.Tensor
 
 
-# class PaddedDatagenerator(BaseDataIterator):
-#     # again, we inherit everything from the baseclass
-#     def __init__(self, dataset: BaseDataset, batchsize: int) -> None:
-#         # we initialize the super class BaseDataIterator
-#         # we now have everything the BaseDataIterator can do, for free
-#         super().__init__(dataset, batchsize)
-    
-#     def __next__(self) -> Tuple[Tensor, Tensor]:
-#         if self.index <= (len(self.dataset) - self.batchsize):
-#             X, Y = self.batchloop()
-#             # we just want to add padding
-#             X_ = pad_sequence(X, batch_first=True, padding_value=0)  # noqa N806
-#             return X_, torch.tensor(Y)
-#         else:
-#             raise StopIteration
+class BaseDataset:
+    """The main responsibility of the Dataset class is to load the data from disk
+    and to offer a __len__ method and a __getitem__ method
+    """
 
-# class BaseDataIterator:
-#     def __init__(self, datapath):
-#         self.data = datapath
-       
-#         self.dataset = dataset
-#         self.batchsize = batchsize
-#     def __len__(self) -> int:
-#         # the lenght is the amount of batches
-#         return int(len(self.dataset) / self.batchsize)
+    def __init__(self, datapath: Path) -> None:
+        self.path = datapath
+        self.data = self.process_data()
 
-#     def proces_data(self)
-#      data = arff.loadarff(datapath)
+    def process_data(self) -> None:
+        data = arff.loadarff(self.path)
+        cur_label = int(data[0][0][14])
+        EEG_list = []  # Lege list voor observaties
+        chunks = []  # Lege list voor chunks
+        for obs in data[0]:
+            if int(obs[14]) == cur_label:
+                EEG_dim = [] # Lege list voor observaties binnen de forloop (behalve de labels)
+                for index, i in enumerate(obs):
+                    if index != 14:
+                        EEG_dim.append(i)
+                EEG_dim = torch.Tensor(EEG_dim)
+                EEG_list.append(EEG_dim)
+            else:
+                chunks_label = (cur_label, torch.stack(EEG_list))
+                chunks.append(chunks_label)
+                cur_label = int(obs[14])
+                EEG_list = []
+                EEG_dim = [] # Lege list voor observaties binnen de forloop (behalve de labels)
+                for index, i in enumerate(obs):
+                    if index != 14:
+                        EEG_dim.append(i)
+                EEG_dim = torch.Tensor(EEG_dim)
+                EEG_list.append(EEG_dim)
+        chunks_label = (cur_label, torch.stack(EEG_list))
+        chunks.append(chunks_label)
+        return chunks
 
-#     def __iter__(self) -> BaseDataIterator:
-#         # initialize index
-#         self.index = 0
-#         self.index_list = torch.randperm(len(self.dataset))
-#         return self
-    
-#     def batchloop(self) -> Tuple[Tensor, Tensor]:
-#         X = []  # noqa N806
-#         Y = []  # noqa N806
-#         # fill the batch
-#         for _ in range(self.batchsize):
-#             x, y = self.dataset[int(self.index_list[self.index])]
-#             X.append(x)
-#             Y.append(y)
-#             self.index += 1
-#         return X, Y
+    def __len__(self) -> int:
+        return len(self.data)
 
-#     def __next__(self) -> Tuple[Tensor, Tensor]:
-#         if self.index <= (len(self.dataset) - self.batchsize):
-#             X, Y = self.batchloop()
-#             return X, Y
-#         else:
-#             raise StopIteration
+    def __getitem__(self, idx: int) -> Tuple:
+        return self.data[idx]
 
 
-# class PaddedDatagenerator(BaseDataIterator):
-#     # again, we inherit everything from the baseclass
-#     def __init__(self, dataset: BaseDataset, batchsize: int) -> None:
-#         # we initialize the super class BaseDataIterator
-#         # we now have everything the BaseDataIterator can do, for free
-#         super().__init__(dataset, batchsize)
-    
-#     def __next__(self) -> Tuple[Tensor, Tensor]:
-#         if self.index <= (len(self.dataset) - self.batchsize):
-#             X, Y = self.batchloop()
-#             # we just want to add padding
-#             X_ = pad_sequence(X, batch_first=True, padding_value=0)  # noqa N806
-#             return X_, torch.tensor(Y)
-#         else:
-#             raise StopIteration
+class BaseDataIterator_wind:
+    """This iterator will consume all data and stop automatically.
+    The dataset should have a:
+        __len__ method
+        __getitem__ method
 
-# class Dataloader:
-#     """Point the dataloader to a directory.
-#     It will load all files, taking the subfolders as classes.
-#     Only files in the formatslist are kept.
-#     The .datagenerator returns an Iterator of batched images and labels
-#     """
+    """
 
-#     def __init__(
-#         self,
-#         path: Path,
-#     ) -> None:
-#         """
-#         Initializes the class
+    def __init__(self, dataset: BaseDataset, window_size: int) -> None:
+        self.dataset = dataset
+        self.ws = window_size
+        self.data = self.window()
 
-#         Args:
-#             path (Path): location of the images
-#             formats (List[str], optional): Formats to keep. Defaults to [".png", ".jpg"]
-#         """
+    def __len__(self) -> int:
+        return int(len(self.data))
 
-#         # get all paths
-#         self.paths, self.class_names = iter_valid_paths(path, formats)
-#         # make a dictionary mapping class names to an integer
-#         self.class_dict: Dict[str, int] = {
-#             k: v for k, v in zip(self.class_names, range(len(self.class_names)))
-#         }
-#         # unpack generator
-#         self.valid_files = [*self.paths]
-#         self.data_size = len(self.valid_files)
-#         self.index_list = [*range(self.data_size)]
+    def window(self) -> None:
+        data = self.dataset
+        list2 = []
+        ws = self.ws
+        for i in range(len(self.dataset)):
+            n_window = len(data[i][1])
+            time = torch.arange(0, ws).reshape(1, -1)
+            window = torch.arange(0, n_window).reshape(-1, 1)
+            idx = time + window
+            idx = idx - ws + 1
+            test = data[i][1][idx]
+            test2 = (data[i][0], test)
+            list2.append(test2)
+        return list2
 
-#         random.shuffle(self.index_list)
+    def __getitem__(self, idx: int) -> Tuple:
+        return self.data[idx]
 
-#         n_train = int(self.data_size * split)
-#         self.train = self.index_list[:n_train]
-#         self.test = self.index_list[n_train:]
 
-#     def __len__(self) -> int:
-#         return len(self.valid_files)
+class BaseDataIterator_pad:
+    """This iterator will consume all data and stop automatically.
+    The dataset should have a:
+        __len__ method
+        __getitem__ method
 
-#     def data_generator(
-#         self,
-#         batch_size: int,
-#         image_size: Tuple[int, int],
-#         channels: int,
-#         channel_first: bool,
-#         mode: str,
-#         shuffle: bool = True,
-#         transforms: Optional[Callable] = None,
-#     ) -> Iterator:
-#         """
-#         Builds batches of images
+    """
 
-#         Args:
-#             batch_size (int): _description_
-#             image_size (Tuple[int, int]): _description_
-#             channels (int): _description_
-#             shuffle (bool, optional): _description_. Defaults to True.
+    def __init__(self, dataset: BaseDataset, window_size: int) -> None:
+        self.dataset = dataset
+        self.ws = window_size
+        self.data = self.padding()
 
-#         Yields:
-#             Iterator: _description_
-#         """
-#         if mode == "train":
-#             data_size = len(self.train)
-#             index_list = self.train
-#         else:
-#             data_size = len(self.test)
-#             index_list = self.test
+    def __len__(self) -> int:
+        return len(self.data)
 
-#         index = 0
-#         while True:
-#             # prepare empty matrices
-#             X = torch.zeros(  # noqa: N806
-#                 (batch_size, channels, image_size[0], image_size[1])
-#             )  # noqa: N806
-#             Y = torch.zeros(batch_size, dtype=torch.long)  # noqa: N806
+    def padding(self) -> None:
+        data = self.dataset
+        window_size = self.ws
+        list_padded = []
+        for i in range(len(data)):
+            len_chunck = len(data[i][1])
+            diff = len_chunck % window_size
+            pad_value = window_size - diff
+            if diff != 0:
+                new_data = F.pad(
+                    input=data[i][1], pad=(0, 0, 0, pad_value), mode="constant", value=0
+                )
+                new_data2 = (data[i][0], new_data)
+                list_padded.append(new_data2)
+            else:
+                new_data3 = (data[i][0], data[i][1])
+                list_padded.append(new_data3)
+        return list_padded
 
-#             for i in range(batch_size):
-#                 if index >= data_size:
-#                     index = 0
-#                     if shuffle:
-#                         random.shuffle(index_list)
-#                 # get path
-#                 file = self.valid_files[index_list[index]]
-#                 # get image from disk
-#                 if transforms is not None:
-#                     img = self.load_image(file, image_size, channels)
-#                     X[i] = transforms(img)
-#                 else:
-#                     X[i] = torch.tensor(self.load_image(file, image_size, channels))
-#                 # map parent directory name to integer
-#                 Y[i] = self.class_dict[file.parent.name]
-#                 index += 1
+    def __getitem__(self, idx: int) -> Tuple:
+        return self.data[idx]
 
-#             if not channel_first:
-#                 X = torch.permute(X, (0, 2, 3, 1))  # noqa N806
 
-#             yield ((X, Y))
+class BaseDataIterator_pad_wind:
+    """This iterator will consume all data and stop automatically.
+    The dataset should have a:
+        __len__ method
+        __getitem__ method
+
+    """
+
+    def __init__(self, dataset: BaseDataset, window_size: int) -> None:
+        self.dataset = dataset
+        self.ws = window_size
+        self.data = self.padding_wind()
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def padding_wind(self) -> None:
+        data = self.dataset
+        window_size = self.ws
+        list2 = []
+        list_padded = []
+        for i in range(24):
+            len_chunck = len(data[i][1])
+            diff = len_chunck % window_size
+            pad_value = window_size - diff
+            if diff != 0:
+                new_data = F.pad(
+                    input=data[i][1], pad=(0, 0, 0, pad_value), mode="constant", value=0
+                )
+                new_data2 = (data[i][0], new_data)
+                list_padded.append(new_data2)
+                n_window = len(list_padded[i][1])
+                time = torch.arange(0, window_size).reshape(1, -1)
+                window = torch.arange(0, n_window).reshape(-1, 1)
+                idx = time + window
+                idx = idx - window_size + 1
+                test = list_padded[i][1][idx]
+                test2 = (list_padded[i][0], test)
+                list2.append(test2)
+            else:
+                new_data3 = (data[i][0], data[i][1])
+                list_padded.append(new_data3)
+                n_window = len(list_padded[i][1])
+                time = torch.arange(0, window_size).reshape(1, -1)
+                window = torch.arange(0, n_window).reshape(-1, 1)
+                idx = time + window
+                idx = idx - window_size + 1
+                test = list_padded[i][1][idx]
+                test2 = (list_padded[i][0], test)
+                list2.append(test2)
+        return list2
+
+    def __getitem__(self, idx: int) -> Tuple:
+        return self.data[idx]
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,37 +9,22 @@ from scipy.io import arff
 from src.data import data_tools
 
 
-
-
-import gin
 import numpy as np
 import pandas as pd
 import requests
 import tensorflow as tf
 import torch
 from loguru import logger
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
 
 
-Tensor = torch.Tensor
-
-
-
-
-def get_eeg(data_dir: Path= "../../data/raw") -> Path:
-    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff"  # noqa: E501
+def get_eeg(data_dir: Path = "../data/raw") -> Path:
+    dataset_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00264/EEG%20Eye%20State.arff"  # noqa: E501
     datapath = tf.keras.utils.get_file(
-        "eeg", origin=url, untar=False, cache_dir=data_dir
+        "eeg_data", origin=dataset_url, untar=False, cache_dir=data_dir
     )
 
     datapath = Path(datapath)
     logger.info(f"Data is downloaded to {datapath}.")
-    
-    
     return datapath
-
-
 
 
